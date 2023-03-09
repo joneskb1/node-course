8f2e9d287d73de2ab72bbb6b2357465a794ca14a
kayla <kayla3445@yahoo.com>
2023-03-09 11:56:13 -0600

    testing webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -67,16 +67,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckoutWebhook = async (session) => {
-  try {
-    const tour = session.client_reference_id;
-    const user = await User.findOne({ email: session.customer_email })._id;
-    // const price = session.line_items[0].price_data.unit_amount / 100;
-    const price = session.amount_total / 100;
-    console.log(tour, user, price);
-    await Booking.create({ tour, user, price });
-  } catch (err) {
-    console.log(err);
-  }
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  // const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
